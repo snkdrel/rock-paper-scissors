9b208f964b7dafaa1ef3272ca9bcd07565e40795
snkdrel <ces.zop@gmail.com>
2022-05-10 15:16:55 -0500

    Add function to play one round
    Function receives player selection and computer selection. It returns
    a message with the round result. Add two constant variables to test the
    function.

--- main.js
+++ main.js
@@ -2,10 +2,29 @@ let computerPlay = function(){
     let randNumber = Math.floor(Math.random() * 3);
     
     if(randNumber == 0){
-        return "Rock";
+        return "rock";
     }else if(randNumber == 1){
-        return "Paper";
+        return "paper";
     }else{
-        return "Scissors";
+        return "scissors";
     }
-}
+};
+
+let playRound = (playerChoice, computerChoice) => {
+    let resultMsg;
+    if(playerChoice == computerChoice){
+        resultMsg = "Draw!";
+    }else if((playerChoice == "rock" && computerChoice == "scissors") ||
+            (playerChoice == "scissors" && computerChoice == "paper") ||
+            (playerChoice == "paper" && computerChoice == "rock")){
+                resultMsg = "You win! " + playerChoice + " beats " + computerChoice;
+            }
+    else{
+        resultMsg = "You loose! " + computerChoice + " beats " + playerChoice;
+    }
+    return resultMsg;
+};
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
